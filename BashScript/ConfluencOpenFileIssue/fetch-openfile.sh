72b3b7362c83440596c86d30a32e790be98264e8
kassem <asem_amr@ymail.com>
2023-03-01 18:44:25 +0200

    add bash script to solve conluence open file issue

--- BashScript/ConfluencOpenFileIssue/fetch-openfile.sh
+++ BashScript/ConfluencOpenFileIssue/fetch-openfile.sh
@@ -5,7 +5,7 @@ x=$(systemctl show --property MainPID confluence.service | cut -c 9-13)
 y=$(lsof -p $x | wc -l)
 if [ $y -gt 12000 ]; then
     echo "too many open file ${y} on $(date)" >>/home/centos/log-opefile/output-issue.log
-    aws ses send-email --from Atlassian@bluebeam.com --destination file:///home/centos/ses/destination.json --message file:///home/centos/ses/message.json
+    aws ses send-email --from Atlassian@ss.com --destination file:///home/centos/ses/destination.json --message file:///home/centos/ses/message.json
 else
     echo "everything is fine and the open file number is ${y} on $(date)" >>/home/centos/log-opefile/output.log
 fi
